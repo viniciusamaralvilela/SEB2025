33936c0e82932086c151bc4942c18f20a979417b
viniciusamaralvilela <amaralvilelavinicius@gmail.com>
2025-05-30 09:28:27 -0300

    organizar

---
+++ projetointegrado/Debug/Core/Src/ST7789/DISPLAY-IPS-main/ST7789/fonts.d
@@ -0,0 +1,5 @@
+Core/Src/ST7789/DISPLAY-IPS-main/ST7789/fonts.o: \
+ ../Core/Src/ST7789/DISPLAY-IPS-main/ST7789/fonts.c \
+ ../Core/Src/ST7789/DISPLAY-IPS-main/ST7789/fonts.h
+
+../Core/Src/ST7789/DISPLAY-IPS-main/ST7789/fonts.h:
---
+++ projetointegrado/Debug/Core/Src/ST7789/DISPLAY-IPS-main/ST7789/images.d
@@ -0,0 +1,5 @@
+Core/Src/ST7789/DISPLAY-IPS-main/ST7789/images.o: \
+ ../Core/Src/ST7789/DISPLAY-IPS-main/ST7789/images.c \
+ ../Core/Src/ST7789/DISPLAY-IPS-main/ST7789/images.h
+
+../Core/Src/ST7789/DISPLAY-IPS-main/ST7789/images.h:
